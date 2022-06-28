a61edd40e76efc0042ffe98f3a7e988c6a74661f
MrManu <superbrandon2018@gmail.com>
2022-06-28 00:04:21 -0500

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import MainLayout from '../src/layout/MainLayout'
+import { ProviderAuth } from '../src/hooks/useAuth'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProviderAuth>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ProviderAuth>
+
+  )
 }
 
 export default MyApp
